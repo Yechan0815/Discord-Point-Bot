1ae0192cea89d599a8f1fcf1d21c12e0e97605dd
yahong <yahong@student.42seoul.kr>
2021-07-16 14:05:22 +0900

    upload

---
+++ Discord-Point-Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord_Point_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
