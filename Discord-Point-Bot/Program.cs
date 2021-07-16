2cdef21bbc24bcdbbce4bae2df735d51323df81d
yahong <yahong@student.42seoul.kr>
2021-07-16 15:43:47 +0900

    Entry: program.cs

--- Discord-Point-Bot/Program.cs
+++ Discord-Point-Bot/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 namespace Discord_Point_Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient _client;
+
+        public static void Main()
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "");
         }
     }
 }
